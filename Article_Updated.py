a6df5fd0a822885442531c8b5703abe284f92d94
Arun-9 <117508413+arun-reddy-a@users.noreply.github.com>
2025-12-10 04:04:31 -0500

    Update Article_Updated.py

--- Article_Updated.py
+++ Article_Updated.py
@@ -18,7 +18,7 @@ def display_gif_frame(gif_path, frame_index):
         st.error(f"Error: {e}")
 
 st.header("Uncertainity in Deep Learning for Regresssion.")
-names = "Anugu Vijay Reddy"
+names = "Anugu Arun Reddy"
 formatted_names = f'<div style="text-align: center;"><strong style="font-size: 17px;">{names}</strong></div>'
 st.write(formatted_names, unsafe_allow_html=True)
 st.write("\n")
